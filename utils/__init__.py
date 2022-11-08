256af78f4e8ee1e4332a88f7ed3797619d73d8be
rborque <ruth.borque.gallego@accenture.com>
2022-11-08 20:21:16 +0100

    Eliminado import de clase Solver

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,4 @@ from utils.properties import properties_package as pack_prop, properties_charger
 from utils.input.create_data import df_centers, df_m30
 from utils.input.create_points import Points
 from utils.input.create_packages import Package
-from utils.problem_solver.problem_solver import Solver
 from utils.input.create_input import GetInput
